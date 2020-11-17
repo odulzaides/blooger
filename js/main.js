9d0796228d896c4b3d9f0050445342e178b73bbf
Oscar Dulzaides <tuti@Oscars-MacBook-Pro.local>
2020-11-16 18:33:35 -0800

    Navbar complete. Beggining main section.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+/*------------- variables --------------------- */
+$nav = $('.nav');
+$toggleCollapse = $('.toggle-collapse');
+
+/*----------x-- variables ----x---------------- */
+
+//  click event 
+$toggleCollapse.click(function(){
+    $nav.toggleClass('collapse')
+})
+
+
+});
